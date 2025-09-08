7a2be8612a0eb588ced4b7b134f54fe74057ec0b
Ujjwal0004 <ujjwalkhadka018@gmail.com>
2025-09-07 21:34:20 -0500

    Added comments

--- Assignment1.py
+++ Assignment1.py
@@ -3,14 +3,14 @@ def insertion_sort(A):
     for i in range(1, len(A)):
         key = A[i]
         j = i - 1
-       
+       # Moving smaller numbers/elements to the right
         while j >= 0 and A[j] < key:
             A[j + 1] = A[j]
             j = j - 1
 
         A[j + 1] = key
     return A
-
+# Testing the code
 numbers = [5,2,9,1,5,1]
 print("Original array:", numbers)
 insertion_sort(numbers)
